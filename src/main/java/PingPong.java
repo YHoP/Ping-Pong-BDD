09669f34797b854dfe03a0acd7f04f97093507ee
YHoP <ypeng.tw@gmail.com>
2015-08-14 19:57:05 -0700

    Update PingPong.java
    Update the functional file

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,40 +1,30 @@
 import java.util.*;
-import java.util.ArrayList;
+import java.io.Console;
 
 public class PingPong {
-
     public static void main(String [] args){
-        // Ask user to input a number
-        // System.out.println("Please input a number: ");
-
-        // Store user input
-        // Scanner input = new Scanner(System.in);
-
-        // Taking String input from user
-        // String userInput = input.nextLine();
-        String userInput = "15";
-
-        // Convert String to Integer
-        Integer number = Integer.parseInt(userInput);
+        System.out.printf("Please input a number: ");
+        Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();		
+        // int number = Integer.parseInt(userInput);
+        System.out.println(isPingPong(number));
     }
 
-    public static ArrayList<String> isPingPong (Integer number){
+    public static ArrayList<String> isPingPong (int number){
         // Create an ArrayList
         ArrayList<String> list = new ArrayList<String>();
-
-        // For loop
-        for (Integer j =1; j <= number; j++){
-            if (number % 3 == 0 || number % 5 == 0){
+        // Create a loop to replace the number with corresponding text
+        for (int i =1; i <= number; i++){
+            if (i % 3 == 0 && i % 5 == 0){
                 list.add("Ping-Pong");
-            }if (number % 3 == 0){
+            }else if (i % 3 == 0){
                 list.add("Ping");
-            }else if (number % 5 == 0){
+            }else if (i % 5 == 0){
                 list.add("Pong");
             }else {
-                list.add(number.toString());
+                list.add(Integer.toString(i));
             }
         }
-
         //return ArrayList
         return list;
     }
